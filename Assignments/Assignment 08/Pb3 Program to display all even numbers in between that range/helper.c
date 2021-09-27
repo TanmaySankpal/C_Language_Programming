1793d49beccb7999fdd18a6a12109cf61a67d88b
Tanmay Sankpal <tanmaysankpal119@gmail.com>
2021-09-27 19:51:00 +0530

    Add files via upload

---
+++ Assignments/Assignment 08/Pb3 Program to display all even numbers in between that range/helper.c
@@ -0,0 +1,34 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//  Function name :     RangeSumEven
+//  Input :             Integer,Integer
+//  Output :            Integer
+//  Description :       It used to print Sum Range between
+//  Author :            Tanmay
+//  Date :              20/09/2021
+//
+//////////////////////////////////////////////////////////////////////////
+
+#include "header.h"
+int RangeSumEven(int iStart,int iEnd)
+{
+	int iSum=0;
+	
+	if(iStart<0||iEnd<=0)
+	{
+		printf("You enter negative number");
+		return 0;
+	}
+	
+	if(iStart>iEnd)
+	{
+		printf("You enter wrong input,%d is grater than %d",iStart,iEnd);
+		return 0;
+	}
+
+	for(iStart;iStart<=iEnd;iStart++)
+	{
+		iSum=iSum+iStart;
+	}
+	return iSum;
+}
