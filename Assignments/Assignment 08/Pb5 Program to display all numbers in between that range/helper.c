0d1091892576cf1580bba34569a08cca581625e3
Tanmay Sankpal <tanmaysankpal119@gmail.com>
2021-09-28 21:08:36 +0530

    Add files via upload

---
+++ Assignments/Assignment 08/Pb5 Program to display all numbers in between that range/helper.c
@@ -0,0 +1,25 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//  Function name :     RangeDisplay
+//  Input :             Integer,Integer
+//  Output :            void(nothing)
+//  Description :       It used to print Range between
+//  Author :            Tanmay
+//  Date :              20/09/2021
+//
+//////////////////////////////////////////////////////////////////////////
+
+#include "header.h"
+void RangeDisplay(int iStart,int iEnd)
+{
+	if(iStart>iEnd)
+	{
+		printf("You enter wrong input,%d is grater than %d",iStart,iEnd);
+		return;
+	}
+
+	for(iEnd;iEnd>=iStart;iEnd--)
+	{
+		printf("%d\t",iEnd);
+	}
+}
