503b255e9bf0f0e157b00e02f2ce811756238a21
Tanmay Sankpal <tanmaysankpal119@gmail.com>
2021-09-29 22:04:52 +0530

    Add files via upload

---
+++ Assignments/Assignment 09/Pb2 Program to It used to print Pattern/helper.c
@@ -0,0 +1,20 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//  Function name :     pattern
+//  Input :             Integer
+//  Output :            Void(Nothing)
+//  Description :       It used to print Pattern
+//  Author :            Tanmay
+//  Date :              22/09/2021
+//
+//////////////////////////////////////////////////////////////////////////
+
+#include "header.h"
+
+void pattern(int iNo)
+{
+	for(;iNo!=0;iNo--)
+	{
+		printf("%d\t#\t",iNo);
+	}
+}
