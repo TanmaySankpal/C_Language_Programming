b12d331dad7f9a8779cabc66d77e9d25d2bcc007
Tanmay Sankpal <tanmaysankpal119@gmail.com>
2021-09-30 23:40:09 +0530

    Add files via upload

---
+++ Assignments/Assignment 09/Pb3 Program to Accept number from user and display pattern/helper.c
@@ -0,0 +1,21 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//  Function name :     pattern
+//  Input :             Integer
+//  Output :            Void(Nothing)
+//  Description :       It used to print Pattern
+//  Author :            Tanmay
+//  Date :              22/09/2021
+//
+//////////////////////////////////////////////////////////////////////////
+
+#include "header.h"
+
+void pattern(int iNo)
+{
+	int iCnt=0;
+	for(iCnt=1;iCnt<=iNo;iCnt++)
+	{
+		printf("%d\t*\t",iCnt);
+	}
+}
