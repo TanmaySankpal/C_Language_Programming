070ce11cdba4a88c92519ea85b57073e59631bd0
Tanmay Sankpal <tanmaysankpal119@gmail.com>
2021-10-01 14:24:29 +0530

    Add files via upload

---
+++ Command Line Argument/hello.c
@@ -0,0 +1,26 @@
+ #include<stdio.h>
+ #include<stdlib.h>
+ 
+ int main(int argc,char *argv[])
+ {
+	 int i=0;
+	 printf("Toatal number of command line arguments are:%d\n",argc);
+	 for(i=0;i<argc;i++)
+	 {
+		 printf("%s\n",argv[i]);
+	 }
+	 return 0;
+ }
+ 
+ //argc for arguments count
+ //argv for name of array (array of character Pointr)  
+ 
+ //Compile-->gcc hello.c -o Myexe
+ //Run-->Myexe Demo 11 Tanmay 21
+ 
+ //Output->Toatal number of command line arguments are:5
+ //			Myexe
+//			Demo
+//			11
+//			Tanmay
+//			21
