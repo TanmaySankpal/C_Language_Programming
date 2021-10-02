2cd04a83556bf490122272119186e7eac002fd06
Tanmay Sankpal <tanmaysankpal119@gmail.com>
2021-10-02 21:52:38 +0530

    Add files via upload

---
+++ Assignments/Assignment 09/Pb5 Program to Accept number from user and display pattern/main.c
@@ -0,0 +1,15 @@
+/////////////////////////////////////////////////////////////////
+//
+// 	Accept number from user and display pattern
+//
+//////////////////////////////////////////////////////////////////
+
+#include "header.h"
+int main()
+{
+	int iValue=0;
+	printf("Enter number of element : ");
+	scanf("%d",&iValue);
+	pattern(iValue);
+	return 0;
+}
