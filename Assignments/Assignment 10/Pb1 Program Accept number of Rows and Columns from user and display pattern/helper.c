d5634387b679cbbb2e26755c2f6cba4bedb02a3c
Tanmay Sankpal <tanmaysankpal119@gmail.com>
2021-10-02 21:59:05 +0530

    Add files via upload

---
+++ Assignments/Assignment 10/Pb1 Program Accept number of Rows and Columns from user and display pattern/helper.c
@@ -0,0 +1,32 @@
+//////////////////////////////////////////////////////////////////////////
+//
+//  Function name :     Pattern
+//  Input :             Integer, Integer
+//  Output :            Void(Nothing)
+//  Description :       It used to Print Pattern
+//  Author :            Tanmay
+//  Date :              27/09/2021
+//
+//////////////////////////////////////////////////////////////////////////
+//
+//		Rows=4  column=4
+//		*	*	*	*
+//		*	*	*	*
+//		*	*	*	*
+//		*	*	*	*
+/////////////////////////////////////////////////////////////////////////
+
+#include"Headder.h"
+void Pattern(int iRow,int iCol)
+{
+	int iCnt=0,i=0;
+	for(iCnt=1;iCnt<=iRow;iCnt++)
+	{
+		for(i=1;i<=iCol;i++)
+		{
+			printf("*\t");
+			
+		}
+		printf("\n");
+	}
+}
