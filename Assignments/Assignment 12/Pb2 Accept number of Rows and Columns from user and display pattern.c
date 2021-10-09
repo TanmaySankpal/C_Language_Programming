eb1277e7ee4456854e7e9b8365bcca3c6ff344b3
Tanmay Sankpal <tanmaysankpal119@gmail.com>
2021-10-09 20:08:52 +0530

    Add files via upload

---
+++ Assignments/Assignment 12/Pb2 Accept number of Rows and Columns from user and display pattern.c
@@ -0,0 +1,38 @@
+/////////////////////////////////////////////////////////////////
+//
+//	Accept number of Rows and Columns from user and display pattern
+//
+///////////////////////////////////////////////////////////////
+#include<stdio.h>
+
+void Pattern(int iRow,int iCol)
+{
+	int i=0,j=0;
+	
+	for(i=1;i<=iRow;i++)
+	{
+		for(j=1;j<=iCol;j++)
+		{
+			if(i+j<iRow+2)
+			{
+				printf("*\t");
+			}
+			else
+			{
+				printf("#\t");
+			}
+			
+		}
+		
+		printf("\n");
+	}
+}
+
+int main()
+{
+	int iRow=0,iCol=0;
+	printf("Enter number of rows and columns\n");
+	scanf("%d%d",&iRow,&iCol);
+	Pattern(iRow,iCol);
+	return 0;
+}
