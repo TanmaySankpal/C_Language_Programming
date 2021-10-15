6907621b04ab7853755bdfbabe8594753554408d
Tanmay Sankpal <tanmaysankpal119@gmail.com>
2021-10-15 20:11:20 +0530

    Add files via upload

---
+++ Assignments/Assignment 15/Pb4 Program to accept number of Rows and Columns from user and display pattern.c
@@ -0,0 +1,42 @@
+/////////////////////////////////////////////////////////////////
+//
+//	Accept number of Rows and Columns from user and display pattern
+//
+///////////////////////////////////////////////////////////////
+#include<stdio.h>
+
+void Pattern(int iRow,int iCol)
+{
+	int i=1,j=1;
+	
+	for(i=1;i<=iRow;i++)
+	{
+		for(j=1;j<=iCol;j++)
+		{
+			if (i==1||j==1||i==iRow||j==iCol||(i+j)==(iCol+1))
+			{
+				printf("*\t");
+			}
+			else if((i+j)<=(iCol))
+			{
+				printf("#\t");
+			}
+			else 
+			{
+				printf("$\t");
+			}
+			
+		}
+		
+		printf("\n\n");
+	}
+}
+
+int main()
+{
+	int iRow=0,iCol=0;
+	printf("Enter number of rows and columns\n");
+	scanf("%d%d",&iRow,&iCol);
+	Pattern(iRow,iCol);
+	return 0;
+}
