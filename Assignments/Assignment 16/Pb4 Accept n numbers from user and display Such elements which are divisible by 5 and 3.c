b6df708af275ac2efb740046dd6367f920026b42
Tanmay Sankpal <tanmaysankpal119@gmail.com>
2021-10-16 22:19:47 +0530

    Add files via upload

---
+++ Assignments/Assignment 16/Pb4 Accept n numbers from user and display Such elements which are divisible by 5 and 3.c
@@ -0,0 +1,50 @@
+/////////////////////////////////////////////////////////////////
+//
+//	Accept n numbers from user and display
+//	Such elements which are divisible by 5 and 3
+//
+///////////////////////////////////////////////////////////////
+#include<stdio.h>
+#include<stdlib.h>
+void Display(int Arr[],int iLength)
+{
+	int iCnt=0;
+	
+	for(iCnt=0;iCnt<iLength;iCnt++)
+	{
+		if((Arr[iCnt]%5)==0&&(Arr[iCnt]%3)==0)
+		{
+			printf("%d\t",Arr[iCnt]);
+		}
+		
+	}
+}
+int main()
+{
+	int iSize=0,iCnt=0;
+	int *p=NULL;
+	
+	printf("Enter Number of elements\n");
+	scanf("%d",&iSize);
+	
+	p=(int*)malloc(iSize *sizeof(int));
+	
+	if(p==NULL)
+	{
+		printf("Unable to allocate memory..");
+		return -1;
+	}
+	
+	printf("Entr %d elements\n",iSize);
+	
+	for(iCnt=0;iCnt<iSize;iCnt++)
+	{
+		printf("Enter element %d:",iCnt+1);
+		scanf("%d",&p[iCnt]);
+	}
+	
+	printf("elements which is divisible by 5 and 3 are : ");
+	Display(p,iSize);
+	free(p);
+	return 0;
+}
