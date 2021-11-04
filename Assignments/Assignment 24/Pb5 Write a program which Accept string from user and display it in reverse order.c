8b91a7901ef0a4ec4d6ed5e7d0cff0e5a62b4044
Tanmay Sankpal <tanmaysankpal119@gmail.com>
2021-11-04 19:42:58 +0530

    Add files via upload

---
+++ Assignments/Assignment 24/Pb5 Write a program which Accept string from user and display it in reverse order.c
@@ -0,0 +1,42 @@
+//////////////////////////////////////////////////////////////////////////////////////////////
+//
+//	Write a program which Accept string from user and
+//  display it inn reverse order
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+
+void StrRevX(char *Str)
+{
+	  int iCnt=0,i=0;
+	  char ch='\0';
+	  char *End=Str;
+	  
+	  while(*End!='\0')
+	  {
+		  iCnt++;
+		  End++;
+	  }
+	  End--;
+
+	  while(Str<End)
+	  {
+		  ch=*Str;
+		  *Str=*End;
+		  *End=ch;
+		  Str++;
+		  End--;
+	  }
+	
+}
+int main()
+{
+	char Arr[20];
+	
+	printf("Enter String..\n");
+	scanf("%[^'\n']s",&Arr);
+	
+	StrRevX(Arr);
+	printf("Reverse String is : %s",Arr);
+	return 0;
+}
