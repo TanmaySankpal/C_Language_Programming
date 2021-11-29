2ef9eb7ef03bfea9104422e7f03ab339ede7326e
Tanmay Sankpal <tanmaysankpal119@gmail.com>
2021-11-29 21:09:46 +0530

    Update Header.h

--- Assignments/Assignment 01/Pb4 Number is divisible by 5 or not/Header.h
+++ Assignments/Assignment 01/Pb4 Number is divisible by 5 or not/Header.h
@@ -1,6 +1,10 @@
-#include<stdio.h>
+// Header file inclusion
+#include <stdio.h>
+
+//Typedef and #define
 typedef int BOOL;
 #define TRUE 1
 #define FALSE 0
 
+// Function prototype
 BOOL Check(BOOL);
